e23bbdcf5cc11c49790718b88088b037dafea3d4
gauravmadan583 <gauravmadan583@gmail.com>
2022-01-16 20:33:57 +0530

    Pushing on Netlify

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,4 +1,4 @@
-export default (state, action) => {
+const AppReducer = (state, action) => {
     switch(action.type) {
         case 'ADD_TRANSACTION':
             return {
@@ -15,3 +15,4 @@ export default (state, action) => {
         
     }
 }
+export default AppReducer;
